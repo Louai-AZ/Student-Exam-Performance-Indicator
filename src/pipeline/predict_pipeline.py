7968aa5dbdaf6c0439d62b0e64a6899288217334
Louai-AZ <azouni.louai@gmail.com>
2023-04-30 12:23:37 +0200

    deployment setup

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -10,6 +10,7 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
+            
             # model_path=os.path.join("artifacts","model.pkl")
             # preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
             
